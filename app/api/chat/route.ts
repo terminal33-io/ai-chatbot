95b472b660b561db1d63330b8b8f4cb8a59e2b57
MK <manish.yo@gmail.com>
2024-01-23 13:28:56 +0530

    updated api call with backend related changes

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,42 +1,49 @@
 import { AIStream, StreamingTextResponse } from 'ai'
 import { auth } from '@/auth'
 import { getSupabaseClient, nanoid } from '@/lib/utils'
-import { createClient } from '@supabase/supabase-js'
 
 export const runtime = 'edge'
 
-  const apiUrl = process.env.API_URL;
+const apiUrl = process.env.API_URL
+const secret = process.env.APP_SECRET as string
 
-  export async function POST(req: Request) {
-    const json = await req.json()
-    const { messages } = json
-    const message = (messages[messages.length - 1]).content
-    const userId = (await auth())?. user?.id
-    const temperature = '1.2'
-    const model = "gpt-3.5-turbo"
-    const version = "1.0.0"
-    const source = "webapp"
-  
-    if (!userId) {
-      return new Response('Unauthorized', {
-        status: 401
-      })
-    }
-  
+export async function POST(req: Request) {
+  const json = await req.json()
+  const { messages } = json
+  const message = messages[messages.length - 1].content
+  const userId = (await auth())?.user?.id
+  const model = 'gpt-4-1106-preview'
+  const version = '1.0.0'
+  const source = 'webapp'
+
+  if (!userId) {
+    return new Response('Unauthorized', {
+      status: 401
+    })
+  }
+
+  try {
     const response = await fetch(`${apiUrl}/chat`, {
       headers: {
         'Content-Type': 'application/json',
+        'X-SECRET': secret,
       },
       method: 'POST',
       body: JSON.stringify({
         message,
-        temperature,
         model
       })
-    });
+    })
 
 
-    const callback =  {
+
+    if (!response.ok) {
+      return new Response('Error', {
+        status: response.status
+      })
+    }
+
+    const callback = {
       onCompletion: async (completion: string) => {
         //save to database
         const supabase = getSupabaseClient()
@@ -52,27 +59,29 @@ export const runtime = 'edge'
             {
               content: completion,
               role: 'assistant',
-              created_at: createdAt,
+              created_at: createdAt
             }
           ],
-          additional_info:{
+          additional_info: {
             model,
             temperature,
             version,
             source
           }
         }
-        const { error } = await supabase
-          .from('chats')
-          .upsert(payload)
-      
-        if(error) {
-          console.log("error storing chat",error) 
+        const { error } = await supabase.from('chats').upsert(payload)
+
+        if (error) {
+          console.log('error storing chat', error)
         }
       }
     }
+
+    const stream = AIStream(response, undefined, callback)
+
+    return new StreamingTextResponse(stream)
+  } catch (e) {
+    console.log('error', e)
+  }
   
-    const stream = AIStream(response, undefined, callback);
-  
-    return new StreamingTextResponse(stream);
 }
